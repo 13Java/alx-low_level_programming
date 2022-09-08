f9f3743d1c27df069db960b2d1aec8596c0f60e8
Anto Moban <antoineantoine369@gmail.com>
2022-09-08 14:45:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.ho>
+/**
+ *  main - print the string in the put function
+ *
+ *  Descriptin: using the main function
+ *  this program prints "with proper grammar, but the outcome is a piece of art."\n
+ *  Return: 0
+ */
+int main(void)
+{
+printf("wuth proper grammar, but the outcome is a piece of art"
+
+
+
+      )
+}
